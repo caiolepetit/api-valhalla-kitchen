0cc6d50713392374d28d49b352c7527c6d86be19
Caio Le Petit <cdct@gft.com>
2023-07-03 16:07:09 -0300

    First commit

---
+++ src/main/java/br/com/apivalhallakitchen/ApiValhallaKitchenApplication.java
@@ -0,0 +1,13 @@
+package br.com.apivalhallakitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiValhallaKitchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiValhallaKitchenApplication.class, args);
+    }
+
+}
