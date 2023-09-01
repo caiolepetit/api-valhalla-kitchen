e3a696885a6b224a02c1d2fd5f0103fe69d716d6
Caio Le Petit <caio.lepetit@gmail.com>
2023-09-01 03:48:35 -0300

    Alteração nos nomes das tabelas

--- src/main/java/br/com/apivalhallakitchen/adapter/driven/infra/entity/PedidoEntity.java
+++ src/main/java/br/com/apivalhallakitchen/adapter/driven/infra/entity/PedidoEntity.java
@@ -1,17 +1,6 @@
 package br.com.apivalhallakitchen.adapter.driven.infra.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,7 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Table(name = "Pedido")
+@Table(name = "pedido")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -35,6 +24,7 @@ public class PedidoEntity {
     private Long clienteId;
 
     @Column(name = "status")
+    @Builder.Default
     private String status = "Novo";
 
     @ElementCollection
