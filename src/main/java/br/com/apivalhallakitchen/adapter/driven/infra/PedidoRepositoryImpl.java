bd01d56a3e9df45398f3e7d02da0da17cea39591
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2023-09-04 20:42:19 -0300

    Ajustes na busca e ordenação da fila de pedidos

--- src/main/java/br/com/apivalhallakitchen/adapter/driven/infra/PedidoRepositoryImpl.java
+++ src/main/java/br/com/apivalhallakitchen/adapter/driven/infra/PedidoRepositoryImpl.java
@@ -5,9 +5,11 @@ import br.com.apivalhallakitchen.adapter.driven.infra.jpa.PedidoRepositoryJpa;
 import br.com.apivalhallakitchen.adapter.utils.mappers.PedidoMapper;
 import br.com.apivalhallakitchen.core.applications.ports.PedidoRepository;
 import br.com.apivalhallakitchen.core.domain.Pedido;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,6 +46,23 @@ public class PedidoRepositoryImpl implements PedidoRepository {
 
     @Override
     public List<Pedido> buscarFilaPedidos(List<String> status) {
-        return pedidoRepositoryJpa.findByStatusInOrderByStatusDesc(status);
+        List<Pedido> pedidos = new ArrayList<>();
+
+        for (PedidoEntity pedido : pedidoRepositoryJpa.findByStatusIn(status)) {
+            pedidos.add(PedidoMapper.pedidoEntityToPedido(pedido));
+        }
+        pedidos.sort(Comparator.comparing(pedido -> {
+            String statusPedido = pedido.getStatus();
+            if (statusPedido.equals("Pronto")) {
+                return 1;
+            } else if (statusPedido.equals("Em preparação")) {
+                return 2;
+            } else if (statusPedido.equals("Recebido")) {
+                return 3;
+            } else {
+                return 4; // Para outros status não especificados
+            }
+        }));
+        return pedidos;
     }
 }
--- src/main/java/br/com/apivalhallakitchen/adapter/driven/infra/jpa/PedidoRepositoryJpa.java
+++ src/main/java/br/com/apivalhallakitchen/adapter/driven/infra/jpa/PedidoRepositoryJpa.java
@@ -1,13 +1,13 @@
 package br.com.apivalhallakitchen.adapter.driven.infra.jpa;
 
 import br.com.apivalhallakitchen.adapter.driven.infra.entity.PedidoEntity;
-import br.com.apivalhallakitchen.core.domain.Pedido;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PedidoRepositoryJpa extends JpaRepository<PedidoEntity, Long> {
 
-    List<Pedido> findByStatusInOrderByStatusDesc(List<String> status);
+    List<PedidoEntity> findByStatusIn(List<String> status);
 
 }
